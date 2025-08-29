7eae756bcceb47da0fea02e8ee3302ec1126cce4
Astro-MoonArclen <moongaminghcr@gmail.com>
2025-08-29 16:09:16 -0400

    Update bannedwords.js

--- bannedwords.js
+++ bannedwords.js
@@ -1,13 +1,5 @@
-// List of words to delete/ban users for
 const bannedWords = [
-  'fuck',
-  'shit',
-  'ass',
-  'bitch',
-  'damn',
-  'crap',
-  'dick',
-  'piss'
+  'fuck', 'shit', 'ass', 'bitch', 'damn', 'crap', 'dick', 'piss'
 ];
 
 module.exports = bannedWords;
